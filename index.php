6f8f4db69bf2ffac0957b6ec1ef51eaa0caa63f0
Leonardo Bianchi <l.bianchi@team.zerynth.com>
2022-02-11 08:54:57 +0100

    simplified structure

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
 <?php
 
-header("Location:php/readCookie.php");
+if (isset($_COOKIE["lang"])) {
+    $lang = $_COOKIE["lang"];
+    switch ($lang) {
+        case "it":
+            header("Location:../pages/it/index.html");
+            break;
+        case "fr":
+            header("Location:../pages/fr/index.html");
+            break;
+        case "es":
+            header("Location:../pages/es/index.html");
+            break;
+    }
+}else{
+    header("Location:php/welcome.php");
+}
