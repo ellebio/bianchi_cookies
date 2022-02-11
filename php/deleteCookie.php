84ab064ad7da72d4541e4910cd7feabc120b3b71
Leonardo Bianchi <l.bianchi@team.zerynth.com>
2022-02-11 09:43:11 +0100

    general modifications

--- php/deleteCookie.php
+++ php/deleteCookie.php
@@ -1,8 +1,4 @@
 <?php
 
-if (isset($_COOKIE["lang"]) && !empty(trim($_COOKIE["lang"]))) {
-    setcookie("lang");
-    header("Location:welcome.php");
-}else{
-    header("Location:welcome.php");
-}
+setcookie("lang");
+header("Location:welcome.php");
